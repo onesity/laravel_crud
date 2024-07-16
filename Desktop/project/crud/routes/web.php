d66857cf3d5bf677c080c63f823c5a5139538bd2
Nishit Goel <temproryuseit1234@gmail.com>
2024-07-16 18:33:44 +0530

    Implement laravel's inbuilt  breeze authentication system

--- Desktop/project/crud/routes/web.php
+++ Desktop/project/crud/routes/web.php
@@ -1,11 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
 Route::get('/signup',[SignupController::class,'create']);
+
 Route::post('/signup',[SignupController::class,'store']);
 Route::get('/list',[SignupController::class,'index'])->name('list');
 Route::get('/edit/{id}',[SignupController::class,'edit']);
 Route::put('/edit/{id}',[SignupController::class,'update']);
 Route::get('/delete/{id}',[SignupController::class,'destroy']);
+
+
+require __DIR__.'/auth.php';
